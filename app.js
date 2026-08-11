c1a12f3679aae36597f90361b99f38f63aa6f699
wiejskachatkapl-stack <wiejskachatka.pl@gmail.com>
2026-08-11 19:37:28 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 (() => {
-  const APP_VERSION = 'v1040';
+  const APP_VERSION = 'v1041';
   const STORAGE_KEY = 'tourmap_points_v1';
   const PROXIMITY_RADIUS_KEY = 'tourmap_proximity_radius_v1';
   const ALERT_HISTORY_KEY = 'tourmap_alert_history_v1';
   const OSM_ENABLED_KEY = 'tourmap_osm_enabled_v1';
   const USER_DB_KEY = 'tourmap_user_attraction_db_v1';
-  const ATTRACTION_DB_URL = 'data/atrakcje-polska.json?v=1037';
+  const ATTRACTION_DB_URL = 'data/atrakcje-polska.json?v=1041';
   const NOMINATIM_URL = 'https://nominatim.openstreetmap.org/search';
   const ROUTE_MODE_KEY = 'tourmap_route_mode_v1';
   const ROUTE_MODES = {
